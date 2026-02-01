b4117b43ac4c3ce2439c57ef1336a3567bee24a1
Artjom Nan <94981351+Aaroncxc@users.noreply.github.com>
2026-02-01 11:28:47 +0100

    Replace video file and update build artifacts
    Removed thehouse_Multi_Portfolio.mp4 from assets/Mp4 and updated public/videos/thehouse.mp4. Regenerated Next.js build artifacts, including cache, manifest, static chunks, and trace files, reflecting the new video and build state. Also updated the server encryption key.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eb44aef37127b030"; }
+/******/ 		__webpack_require__.h = function() { return "05accff5af5740c4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
